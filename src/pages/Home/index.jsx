f8a7934ba570a62252c066d6b74ae63c9086a721
Brenobn <brenobragabn@gmail.com>
2024-01-29 20:38:11 -0300

    Creating pages and Home folder, with HTML structure and style

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Home() {
+  return(
+    <Container>
+      <Button />
+    </Container>
+  );
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 42.8rem;
+  height: 151.1rem;
+
+  margin: 0 auto;
+
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+`;
