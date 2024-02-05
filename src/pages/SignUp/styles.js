2dbce22d9cb3dac4f9cf5388ce4c64ec5b6e516e
Brenobn <brenobragabn@gmail.com>
2024-02-05 17:38:21 -0300

    Adjusting page style to suit desktop using media quiries

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 42.8rem;
-  height: 92.6rem;
+  width: 100%;
+  height: 100%;
 
   margin: 0 auto;
 
@@ -23,6 +23,16 @@ export const Logo = styled.h1`
     width: 4.3rem;
     height: 4.3rem;
   }
+
+  @media(min-width: 1024px) {
+    font-size: 4.2vw;
+    margin-bottom: 10.0rem;
+
+    > img {
+      width: 4.7vw;
+      height: 4.7vw;
+    }
+  }
 `;
 
 export const LinkToCreateAccount = styled.a`
@@ -35,12 +45,18 @@ export const LinkToCreateAccount = styled.a`
   font-weight: 500;
 `;
 
-export const PageContainer = styled.div`
+export const Main = styled.div`
   display: flex;
   padding: 15.8rem 4.7rem 23.5rem 6.5rem;
   flex-direction: column;
   align-items: center;
   gap: 7.3rem;
+
+  @media(min-width: 1024px) {
+    flex-direction: row;
+    gap: 20.0rem;
+    max-width: fit-content;
+  }
 `;
 
 export const FormContainer = styled.form`
@@ -50,4 +66,22 @@ export const FormContainer = styled.form`
   gap: 3.2rem;
 
   width: 31.6rem;
+
+  @media(min-width: 1024px) {
+    max-width: 48.0vw;
+  }
 `;
+
+export const Title = styled.h2`
+  color: ${({ theme }) => theme.COLORS.SIMPLE_WHITE};
+  text-align: center;
+
+  font-family: 'Poppins', sans-serif;
+  font-size: 3.2vw;
+  font-weight: 500;
+
+  @media(max-width: 1024px) {
+    display: none;
+    font-size: 3.2vw;
+  }
+`;  
